f7f1314ca5039f7fe36caaf39b4a95e8b3a5c545
Stuart Parry <stuart.parry@gmail.com>
2017-10-16 10:02:19 +0100

    Updates to emacs configuration for python/jedi

---
+++ .emacs.d/custom.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("cab317d0125d7aab145bc7ee03a1e16804d5abdfa2aa8738198ac30dc5f7b569" "f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" "7beac4a68f03662b083c9c2d4f1d7f8e4be2b3d4b0d904350a9edf3cf7ce3d7f" "39fe48be738ea23b0295cdf17c99054bb439a7d830248d7e6493c2110bfed6f8" "39dd7106e6387e0c45dfce8ed44351078f6acd29a345d8b22e7b8e54ac25bac4" "308eb04bfba22cd158a0927362d3a138653755eb7e82d626088df845c77b59c6" "1b1e54d9e0b607010937d697556cd5ea66ec9c01e555bb7acea776471da59055" "ba9be9caf9aa91eb34cf11ad9e8c61e54db68d2d474f99a52ba7e87097fa27f5" "aae95fc700f9f7ff70efbc294fc7367376aa9456356ae36ec234751040ed9168" "2dd32048690787844d8cba601ed3dd8b2f419e9bd985898d0c3792671a05b96b" "4980e5ddaae985e4bae004280bd343721271ebb28f22b3e3b2427443e748cd3f" "0c387e27a3dd040b33c6711ff92e13bd952369a788eee97e4e4ea2335ac5528f" default)))
+ '(linum-format "%3i")
+ '(package-selected-packages
+   (quote
+    (vimish-fold auctex haskell-mode helm-cider helm-cider-mode cider jknav folding bpr helm-projectile helm-swoop helm-ls-git helm org-bullets org-evil jedi yasnippet powerline-evil powerline quasi-monochrome-theme noctilux-theme monochrome-theme firebelly-theme busybee-theme bubbleberry-theme abyss-theme evil-search-highlight-persist evil-nerd-commenter evil-indent-textobject evil-visualstar evil-matchit evil-numbers evil-surround evil-leader evil vdiff magit minimap fill-column-indicator linum-relative auto-complete use-package)))
+ '(powerline-color1 "#3d3d68")
+ '(powerline-color2 "#292945"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-candidate-face ((t (:inherit popup-face :background "black" :foreground "gray"))))
+ '(ac-candidate-mouse-face ((t (:inherit popup-menu-mouse-face :background "dark gray" :foreground "black"))))
+ '(ac-selection-face ((t (:inherit popup-menu-selection-face :background "black" :foreground "dark orange"))))
+ '(popup-summary-face ((t (:inherit popup-face :background "black" :foreground "deep sky blue"))))
+ '(popup-tip-face ((t (:background "black" :foreground "chartreuse")))))
