31c203f65c99e4686d8254a815fea31206b17ad7
stuart <stuart@broadcom.com>
2017-10-17 15:00:46 +0100

    Updated emacs configuration so yasnippet plays nicely with AC
      -CTRL+TAB (yas) vs TAB (AC)

--- .emacs.d/lisp/init-yasnippet.el
+++ .emacs.d/lisp/init-yasnippet.el
@@ -7,6 +7,10 @@
 
 (use-package yasnippet
   :ensure t
-  :commands (yas-global-mode))
+  :commands (yas-global-mode)
+  :config
+    (define-key yas-minor-mode-map (kbd "<tab>") nil)
+    (define-key yas-minor-mode-map (kbd "TAB") nil)
+    (define-key yas-minor-mode-map (kbd "<C-tab>") 'yas-expand))
 
 (provide 'init-yasnippet)
